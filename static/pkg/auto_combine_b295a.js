59b4a6ec3fafa692ab8f81831ac30f85737f3ac7
wangling <1102311949@qq.com>
2016-05-24 12:02:43 +0800

    dev

--- static/pkg/auto_combine_b295a.js
+++ static/pkg/auto_combine_b295a.js
@@ -2745,7 +2745,7 @@ var Panel = {
 	_initSel: function() {
 		var self = this;
 
-		self._resetSku();
+		// self._resetSku();
 		amount.init();
 		$('#J-cm').delegate('label', EVENT_TAP, function(evt) {
 			var $cur = $(evt.target);
